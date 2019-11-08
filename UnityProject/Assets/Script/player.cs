b305837275402fa87c0062ebeebd7f0e1b43ff71
27796099 <56706519+27796099@users.noreply.github.com>
2019-11-08 12:01:09 +0800

    2D場景

---
+++ UnityProject/Assets/Script/player.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header("速度"),Range(0f,100f)]
+    public float speed = 3.5f;
+    [Header("跳躍高度"),Range(100,2000)]
+    public int jumpHigh = 300;
+    [Header("是否在地上"),Tooltip("用來判斷角色是否在地上")]
+    bool isGround = false;
+    [Header("玩家名稱")]
+    public string name = "RR";
+
+
+}
