b3ce39f57429803341d963948ffe1cdeab250d24
27796099 <56706519+27796099@users.noreply.github.com>
2019-11-15 12:26:02 +0800

    男孩控制

--- UnityProject/Assets/Script/player.cs
+++ UnityProject/Assets/Script/player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    #region 欄位區域
     [Header("速度"),Range(0f,100f)]
     public float speed = 3.5f;
     [Header("跳躍高度"),Range(100,2000)]
@@ -11,6 +12,49 @@ public class player : MonoBehaviour
     bool isGround = false;
     [Header("玩家名稱")]
     public string name = "RR";
+    [Header("2D 鋼體")]
+    public Rigidbody2D r2d;
+    public Animator  ani ;
+
+    #endregion 
 
 
+
+    private void Move()
+    {
+  
+        float h = Input.GetAxisRaw("Horizontal");
+        r2d.AddForce(new Vector2(speed*h,0));
+        ani.SetBool("跑步開關", h != 0);
+    }
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGround ==true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0, jumpHigh));
+        }
+
+
+
+    }
+    private void Dead()
+    {
+
+
+    }
+    private void Update()
+    {
+        Move();
+        Jump();
+
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.name == "地板")
+        {
+            isGround = true ;
+        }
+    }
 }
